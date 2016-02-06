aedc0bb4ae0fb653f8dbbeebe0be91089fc8eb8b
Matthew Erbs <merbs@splunk.com>
2016-02-06 15:35:46 +1000

    MOAR clean up

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var colors = require('colors');
 var adal=require('adal-node');
 var Client=require('node-rest-client').Client;
 var rest=require('restler');
+var _ = require('lodash');
+var waitUntil = require('wait-until');
+
 var AuthenticationContext= adal.AuthenticationContext;
 
 var clientId = "";
@@ -25,11 +28,14 @@ colors.setTheme({
   error: 'red'
 });
 
-
+var debug = function(str){
+  console.log(colors.debug(str));
+}
+  
 co(function *(){
-  clientId = yield prompt('Enter your client id: '.prompt);
-  tenantId = yield prompt('Enter your tenant id: '.prompt);
-  subscriptionId = yield prompt('Enter your subscription id: '.prompt);
+  clientId = yield prompt('Enter your Azure AD Client id: '.prompt);
+  tenantId = yield prompt('Enter your Tenant Domain id: '.prompt);
+  subscriptionId = yield prompt('Enter your Azure Subscription id: '.prompt);
 
   var resource="https://management.azure.com/";
   var authURL="https://login.windows.net/" + tenantId;
@@ -39,38 +45,24 @@ co(function *(){
   var authorityHostUrl ='https://login.microsoftonline.com';
   var context = new AuthenticationContext(authURL, null, cache);
   var authHeader = "";
-  console.log("Getting a OAuth Token".green);
+
+  console.log("Getting an OAuth Token...");
 
   context.acquireUserCode(resource, clientId, 'es-mx', function (err, response) {
     if (err) {
       console.log(colors.error('Oh dear!, I need to call Paul Bouwer: ' + err.stack));
     } else {
-      console.log(colors.help(response.message.yellow));
+      console.log(colors.info(response.message));
+
+      console.log(colors.info("Go to: http://bit.ly/AzureDeviceAuth"));
 
       context.acquireTokenWithDeviceCode(resource, clientId, response, function (err, tokenResponse) {
         if (err) {
-          console.log('error happens when acquiring token with device code');
-          console.log(err);
+          console.log(colors.error('Oh dear!, I need to call Paul Bouwer: ' + err.stack));
         }
         else {
           authHeader = tokenResponse['accessToken'];
-          console.log(colors.info("Got a token:" + authHeader));
-          console.log(colors.info(""));
-          console.log(colors.info("http://bit.ly/AzureDeviceAuth"));
-
-          // var getrates = function(deal, data){
-          //   requestURL = "https://management.azure.com/subscriptions/"
-          //     + subscriptionId
-          //     + "/providers/Microsoft.Commerce/RateCard?api-version=2015-06-01-preview&$filter=OfferDurableId eq '"
-          //     + deal
-          //     + "' and Currency eq 'USD' and Locale eq 'en-US' and RegionInfo eq 'US'"
-          //
-          //   rest.get(requestURL, {accessToken:authHeader})
-          //     .on('complete',function(result) {
-          //       data = result
-          //       console.log(data);
-          //     });
-          // };
+          debug("Got a token!!!");
 
           var getUrl = function(subscriptionId, deal){
             requestURL = "https://management.azure.com/subscriptions/"
@@ -82,31 +74,119 @@ co(function *(){
             return requestURL;
           }
 
-          msdnUrl = getUrl(subscriptionId, "MS-AZR-0149P");
+          bizsparkUrl = getUrl(subscriptionId, "MS-AZR-0149P");
+          msdnUrl = getUrl(subscriptionId, "MS-AZR-0063P");
           paygUrl = getUrl(subscriptionId, "MS-AZR-0003P");
+
           var payg = "";
           var msdn = "";
+          var bizspark = ""
+          var paygDone = false;
+          var msdnDone = false;
+          var bizsparkDone = false;
 
-          console.log("Getting MSDN rates...")
+          var currentData = ""
+
+
+          var compute = function(){
+            var storageData = _.filter(payg.Meters, function(o) {
+              return  (o.MeterCategory.indexOf("Storage") > -1) || (o.MeterSubCategory.indexOf("Storage") > -1)
+            });
+
+            _.each(storageData, function(entry) {
+              console.log(
+                " When hosted in the "
+                + colors.info(entry.MeterRegion)
+                + " region,  " +
+                colors.info(entry.MeterName)
+                + " for "
+                + colors.info(entry.MeterCategory)
+                + " is going to cost you: "
+              );
+
+              _.each(entry.MeterRates, function(rate){
+                var m = "$" + rate + " USD per (" + entry.Unit + ")";
+                console.log("     - " + m)
+              });
+            });
+          };
+
+          var storage = function(){
+            var storageData = _.filter(payg.Meters, function(o) {
+              return  (o.MeterCategory.indexOf("Storage") > -1) || (o.MeterSubCategory.indexOf("Storage") > -1)
+            });
+
+            _.each(storageData, function(entry) {
+              console.log(
+                " When hosted in the "
+                + colors.info(entry.MeterRegion)
+                + " region,  " +
+                colors.info(entry.MeterName)
+                + " for "
+                + colors.info(entry.MeterCategory)
+                + " is going to cost you: "
+              );
+
+              _.each(entry.MeterRates, function(rate){
+                var m = "$" + rate + " USD per (" + entry.Unit + ")";
+                console.log("     - " + m)
+              });
+            });
+          };
+
+          debug("Getting MSDN rates...")
+          debug("Getting PAYG rates...")
+          debug("Getting BizSpark rates...")
+
           rest.get(msdnUrl, {accessToken:authHeader})
           .on('complete',function(result) {
             msdn = result
-
-            console.log("Getting PAYG rates...")
-
-            rest.get(paygUrl, {accessToken:authHeader})
-            .on('complete',function(result) {
-              payg = result
-
-              var replServer = repl.start({
-                prompt: "baug > ".yellow,
-              });
-
-              replServer.context.msdn = msdn;
-              replServer.context.payg = payg;
-
-            });
+            debug("MSDN Done!");
+            msdnd = true;
           });
+
+          rest.get(paygUrl, {accessToken:authHeader})
+          .on('complete',function(result) {
+            payg = result
+            debug("PAYG Done!");
+            paygDone = true;
+          });
+
+          rest.get(bizsparkUrl, {accessToken:authHeader})
+          .on('complete',function(result) {
+            bizspark = result
+            debug("BizSpark Done!");
+            bizsparkDone = true
+          });
+
+
+          waitUntil()
+              .interval(500)
+              .times(40)
+              .condition(function() {
+                  return bizsparkDone && msdnDone && paygDone;
+              })
+              .done(function(result) {
+
+                var replServer = repl.start({
+                  prompt: "baug > ".yellow,
+                });
+
+                replServer.context.msdn = msdn;
+                replServer.context.payg = payg;
+                replServer.context.bizspark = bizspark;
+                replServer.context.currentData = currentData;
+
+                replServer.context.msdnUrl = msdnUrl;
+                replServer.context.paygUrl = paygUrl;
+                replServer.context.bizsparkUrl = bizsparkUrl;
+
+                replServer.context.storage = storage; //hard coded to PAYG
+                replServer.context.compute = compute; //hard coded to PAY
+
+                replServer.context.offerUrl = "https://azure.microsoft.com/en-us/support/legal/offer-details/";
+
+              });
         }
       });
     }
